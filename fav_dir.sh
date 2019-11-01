07ec826c68c555779a7fc59dcf2079367f021e8c
Ella Carmon <ellacarmon@gmail.com>
2019-11-01 15:56:05 +0200

    added flags

--- fav_dir.sh
+++ fav_dir.sh
@@ -1,29 +1,87 @@
 #!/bin/bash
-
+#
+# Created By: ellacarmon
+# Github: github.com/ellacarmon
+# License: MIT
+# CopyWrite: 2019
+#
+set -e
 echo "############## Welcome ################"
 echo ""
 
+function show_favorites {
+    favorites=(${DIR1} ${DIR2} ${DIR3} ${DIR4} ${DIR5})
+    echo "Here are your TOP5 direcotries"
+    for i in "${!favorites[@]}"; do
+      let index=${i}+1
+      printf '[%s] %s\n' ${index} "${favorites[i]}"
+    done
+}
 
-CONF=config.conf
-if [[  -f ${CONF} ]];
-then
-    echo "Sourcing config file"
-    source ${CONF}
-    echo "sourced ${CONF}"
-else
-    echo "No config file was found"
-    echo "Please provide your top 5 directories"
-    read dir1 dir2 dir3 dir4 dir5 dir6
-
-    echo "DIR1=${dir1} DIR2=${dir2} DIR3=${dir3} DIR4=${dir4} DIR5=${dir5}" | tr " " "\n" > config.conf
+function choose_favorite {
+    echo "Please choose your desired directory"
+    read chosen_dir
+    echo cd ${favorites[$chosen_dir-1]}
+    cd ${favorites[$chosen_dir-1]}
+    }
+function config_file {
+    CONF=config.conf
+    if [[  -f ${CONF} ]];
+    then
+        echo "Sourcing config file"
+        source ${CONF}
+        echo "sourced ${CONF}"
+        echo " "
+    else
+        echo "No config file was found"
+        echo "Please provide your top 5 directories"
+        read dir1 dir2 dir3 dir4 dir5 dir6
+        echo "DIR1=${dir1} DIR2=${dir2} DIR3=${dir3} DIR4=${dir4} DIR5=${dir5}" | tr " " "\n" > ${CONF}
+        source ${CONF}
+    fi
+}
+if [[ -z "$1" ]]
+  then
+     config_file
+     show_favorites
+     choose_favorite
 fi
-
-favorites=(${DIR1} ${DIR2} ${DIR3} ${DIR4} ${DIR5})
-echo "Here are your TOP5 direcotries"
-for i in "${!favorites[@]}"; do
-  printf '[%s] %s\n' "$i" "${favorites[i]}"
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo " "
+      echo "options:"
+      echo "-h,   --help                     show brief help"
+      echo "-nc,  --new_config               change all 5 favorites folders"
+      echo "-cf,  --change_favorite          change a specific folder"
+      exit 0
+      ;;
+    -nc | --new_config)
+      echo "######## entering config mode #########"
+      echo "Please provide your top 5 directories"
+      read dir1 dir2 dir3 dir4 dir5
+      echo "DIR1=${dir1} DIR2=${dir2} DIR3=${dir3} DIR4=${dir4} DIR5=${dir5}" | tr " " "\n" > config.conf
+      echo "done"
+      shift
+      ;;
+    -cf | --change_favorite)
+      config_file
+      echo "######## entering config mode #########"
+      favorites=(${DIR1} ${DIR2} ${DIR3} ${DIR4} ${DIR5})
+      show_favorites
+      echo "which directory you would like to replace?"
+      read dir_index
+      echo "please enter your new favorite directory path"
+      read new_dir
+      export DIR${dir_index}=${new_dir}
+      echo "DIR1=${DIR1} DIR2=${DIR2} DIR3=${DIR3} DIR4=${DIR4} DIR5=${DIR5}"  | tr " " "\n" > config.conf
+      echo ""
+      show_favorites
+      shift
+      ;;
+     *)
+     echo "unexpected argument $1"
+     break
+     ;;
+  esac
 done
-
-echo "Please choose your desired directory"
-read chosen_dir
-cd ${favorites[$chosen_dir]}
---
+++ import.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Created By: ellacarmon
+# Github: github.com/ellacarmon
+# License: MIT
+# CopyWrite: 2019
+#
+echo "############## Welcome ################"
+echo "First argument will be the path to install the script, default is home folder"
+echo "Second argument will be the source profile folder, deafult is ~/.zshrc"
+echo ""
+
+PATH_TO_INSTALL=$HOME/
+
+if [[ -n $1 ]]
+    then
+        PATH_TO_INSTALL=$1
+fi
+
+echo "The path of the script is ${PATH_TO_INSTALL}"
+echo ""
+echo "Cloning script to ${PATH_TO_INSTALL}"
+cd ${PATH_TO_INSTALL}
+git clone https://github.com/ellacarmon/fd.git
+cd fd
+cp fav_dir.sh ${PATH_TO_INSTALL}
+SOURCE_FOLDER=~/.zshrc
+if [[ -n $2 ]]
+    then
+        SOURCE_FOLDER=$2
+fi
+
+echo "source folder is ${SOURCE_FOLDER}"
+echo "adding fav alias"
+ALIAS='alias fav=".'${PATH_TO_INSTALL}'/fav_dir.sh"'
+
+echo ${ALIAS} >> ${SOURCE_FOLDER}
+source ${SOURCE_FOLDER}
+
+
+echo "DONE!"%
